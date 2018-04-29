191c8c9dacf7c4979493e90ee86cd17ef978a478
URANO Ryoya <urano.works.mail@gmail.com>
2018-04-30 00:15:05 +0900

    match式を使ったFizzBuzz

--- src/main/scala/example/FirstFizzBuzz.scala
+++ src/main/scala/example/FirstFizzBuzz.scala
@@ -2,15 +2,15 @@ package example
 
 object FirstFizzBuzz {
   def fizzBuzz(n: Int): Unit = for { i <- 1 to n } {
-    if (i % 15 == 0) {
-      println("FizzBuzz")
-    } else if (i % 3 == 0) {
-      println("Fizz")
-    } else if (i % 5 == 0) {
-      println("Buzz")
-    } else {
-      println(i)
-
+    i match {
+      case x if x % 15 == 0 =>
+        println("FizzBuzz")
+      case x if x % 3 == 0 =>
+        println("Fizz")
+      case x if x % 5 == 0 =>
+        println("Buzz")
+      case x =>
+        println(x)
     }
   }
 
