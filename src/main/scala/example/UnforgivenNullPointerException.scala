2a1289f3099bde39d5326ac9459b98cc8f9a30fb
URANO Ryoya <urano.works.mail@gmail.com>
2018-04-30 16:24:57 +0900

    add 7章　ぬるぽなんて、あたしが許さない

---
+++ src/main/scala/example/UnforgivenNullPointerException.scala
@@ -0,0 +1,10 @@
+// 7章　ぬるぽなんて、あたしが許さない
+package example
+
+object UnforgivenNullPointerException {
+  def main(args: Array[String]): Unit = {
+    val message = "Hello World"
+    println(message)
+  }
+}
+
