bc525fd230620a2841a9d697bc4a0cf4ed604835
URANO Ryoya <urano.works.mail@gmail.com>
2018-04-30 16:05:56 +0900

    Listを使った実装

--- src/main/scala/example/ReFizzBuzz.scala
+++ src/main/scala/example/ReFizzBuzz.scala
@@ -1,9 +1,19 @@
 // 6章　再会のFizzBuzz
 package example
 
-object Hello {
+object ReFizzBuzz {
+  def toFizzBuzz(numbers: List[Int]): List[String] =
+    numbers.map((i: Int) => i match {
+      case x if x % 15 == 0 => "FizzBuzz"
+      case x if x % 3 == 0 => "Fizz"
+      case x if x % 5 == 0 => "Buzz"
+      case x => x.toString
+    })
+
   def main(args: Array[String]): Unit = {
-    val message = "Hello World"
-    println(message)
+    val n =15
+    val numbers = (1 to n).toList
+    val fizzBuzzList = toFizzBuzz(numbers)
+    fizzBuzzList.foreach((s: String) => println(s))
   }
 }
